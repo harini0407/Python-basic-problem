71df2ba2c33a3445043586efbdce8d5379b7ddb7
harini <harinibabu0407@gmail.com>
2024-11-29 21:03:41 +0530

    prime numbers

---
+++ primenumbers.py
@@ -0,0 +1,13 @@
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
+n=int(input())
+for i in range(n):
+    if prime(i):
+        print(i)
+        
