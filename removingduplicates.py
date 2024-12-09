051a1fac4ef9df2829be073f5cf9bad8db599168
harini <harinibabu0407@gmail.com>
2024-12-09 17:57:43 +0530

    sorting a tupke based on second element

--- removingduplicates.py
+++ removingduplicates.py
@@ -6,3 +6,10 @@ for i in range(x):
 
 temp=set(l)
 print(list(temp))
+
+#optimal solution
+'''
+x = int(input("Enter the number of elements: "))
+l = set(int(input("Enter a number: ")) for _ in range(x))
+print(list(l))
+'''
