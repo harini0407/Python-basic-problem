30a956ed056c1cd4bcd8ba4f1f4618a24877375c
harini <harinibabu0407@gmail.com>
2024-12-09 17:58:43 +0530

    sorting a tuple based on second element

--- sortingatuple.py
+++ sortingatuple.py
@@ -1,3 +1,3 @@
 t = (5, 2, 6, 3, 4, 1)
-res = sorted(t, key=lambda x: (x != 2, x))  # custom sorting key
-print(res)  # print the sorted result
+res = sorted(t, key=lambda x: (x != t[1], x))  
+print(res) 
