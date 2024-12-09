910775ec2c95fe0abba066abc85090170e3a67e3
harini <harinibabu0407@gmail.com>
2024-12-09 16:43:30 +0530

    intersection of two sets

--- primenumbers.py
+++ primenumbers.py
@@ -2,7 +2,7 @@ def prime(n):
     if n<2:
         return False
     for i in range(2,n):
-        if n%i==0:
+        if n%i==0:#n=10 i=2 10%2==0 10%3==0 10%4==0 10%5==0
             return False
     return True
 
@@ -10,4 +10,3 @@ n=int(input())
 for i in range(n):
     if prime(i):
         print(i)
-        
---
+++ setintersection.py
@@ -0,0 +1,4 @@
+a={2,3,4,5}
+b={4,5,6,7}
+c=a.intersection(b)
+print(c)
